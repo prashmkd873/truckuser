dfc7c17d4ae29f26862357aaa68d599c9f95fae1
prashmkd873 <prashmkd873@gmail.com>
2026-01-16 11:12:53 +0530

    Update AIdemo.php

--- AIdemo.php
+++ AIdemo.php
@@ -7,7 +7,7 @@
 YOU :<p id="output"></p>
 GOOGJI :<p id="ai"></p>         
 <div id="result"></div>          
-<img onclick="speakText(document.getElementById('ai').innerText);" style= "position:fixed;bottom:2%;right:2%;width:10%;height:7%;border-radius:50%;background:black;padding:10px 10px 10px 10px;" src="speaker.png">
+<img onclick="speakText(document.getElementById('ai').innerText);" style= "position:fixed;bottom:2%;right:2%;width:10%;height:7%;border-radius:50%;background:black;padding:10px 10px 10px 10px;" src="">
           
 <img onclick="startListening()" style= "position:fixed;bottom:5%;right:37%;width:15%;height:10%;border-radius:50%;background:lightgray;padding:20px 20px 20px 20px;" src="micro.png">
 
